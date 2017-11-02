cf000e0a9e831b9921159305afbdd77389ec050e
olxz384 <olxz384@gmail.com>
2017-11-02 16:51:29 -0400

    server, routes, dependecies added

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log(" App is listening on PORT: " + PORT);
+});
