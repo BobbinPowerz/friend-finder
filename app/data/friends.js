b1a94e244140f5b6179f0933a08c0601456f353a
unknown <bobbin.powerz@gmail.com>
2017-11-02 17:49:01 -0700

    made progress on routes,html, server

---
+++ app/data/friends.js
@@ -0,0 +1,483 @@
+var friendMatches = [
+{
+	"name": "Ahmed",
+	"photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	"scores": [
+	"5",
+	"1",
+	"4",
+	"4",
+	"5",
+	"1",
+	"2",
+	"5",
+	"4",
+	"1"
+	]
+},
+{
+	"name": "Jacob Deming",
+	"photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+	"scores": [
+	"4",
+	"2",
+	"5",
+	"1",
+	"3",
+	"2",
+	"2",
+	"1",
+	"3",
+	"2"
+	]
+},
+{
+	"name": "Jeremiah Scanlon",
+	"photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+	"scores": [
+	"5",
+	"2",
+	"2",
+	"2",
+	"4",
+	"1",
+	"3",
+	"2",
+	"5",
+	"5"
+	]
+},
+{
+	"name": "Louis T. Delia",
+	"photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+	"scores": [
+	"3",
+	"3",
+	"4",
+	"2",
+	"2",
+	"1",
+	"3",
+	"2",
+	"2",
+	"3"
+	]
+},
+{
+	"name": "Lou Ritter",
+	"photo": "https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+	"scores": [
+	"4",
+	"3",
+	"4",
+	"1",
+	"5",
+	"2",
+	"5",
+	"3",
+	"1",
+	"4"
+	]
+},
+{
+	"name": "Jordan Biason",
+	"photo": "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+	"scores": [
+	"4",
+	"4",
+	"2",
+	"3",
+	"2",
+	"2",
+	"3",
+	"2",
+	"4",
+	"5"
+	]
+},
+{
+	"name": "dan",
+	"photo": "Pictures",
+	"scores": [
+	"3",
+	"5",
+	"2",
+	"2",
+	"4",
+	"1",
+	"1",
+	"2",
+	"2",
+	"3"
+	]
+},
+{
+	"name": "Ally",
+	"photo": "www.....",
+	"scores": [
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1"
+	]
+},
+{
+	"name": "dan",
+	"photo": "Pictures",
+	"scores": [
+	"3",
+	"5",
+	"2",
+	"2",
+	"4",
+	"1",
+	"1",
+	"2",
+	"2",
+	"3"
+	]
+},
+{
+	"name": "koko",
+	"photo": "https://www.google.com/search?q=men&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjdhKPdwaDXAhUR32MKHRfSDp8Q_AUICigB&biw=1920&bih=949#imgrc=6qL966B7vDoKCM:",
+	"scores": [
+	"1",
+	"5",
+	"2",
+	"2",
+	"4",
+	"1",
+	"1",
+	"2",
+	"2",
+	"3"
+	]
+},
+{
+	"name": "jabaku",
+	"photo": "https://www.google.com/search?q=men&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjdhKPdwaDXAhUR32MKHRfSDp8Q_AUICigB&biw=1920&bih=949#imgrc=WgOWmLCASn43ZM:",
+	"scores": [
+	"3",
+	"5",
+	"2",
+	"2",
+	"4",
+	"1",
+	"1",
+	"2",
+	"2",
+	"3"
+	]
+},
+{
+	"name": "kabuki",
+	"photo": "https://www.google.com/search?q=men&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjdhKPdwaDXAhUR32MKHRfSDp8Q_AUICigB&biw=1920&bih=949#imgrc=cFD0ZqbPwt7hJM:",
+	"scores": [
+	"3",
+	"5",
+	"2",
+	"2",
+	"5",
+	"1",
+	"1",
+	"2",
+	"2",
+	"3"
+	]
+},
+{
+	"name": "Sean",
+	"photo": "Deez",
+	"scores": [
+	"2",
+	"4",
+	"4",
+	"2",
+	"5",
+	"2",
+	"5",
+	"3",
+	"2",
+	"2"
+	]
+},
+{
+	"name": "c",
+	"photo": "s",
+	"scores": [
+	"2",
+	"2",
+	"3",
+	"1",
+	"1",
+	"1",
+	"2",
+	"2",
+	"1",
+	"1"
+	]
+},
+{
+	"name": "Shine Toriama",
+	"photo": "https://i.ytimg.com/vi/jvImc0s1QVY/maxresdefault.jpg",
+	"scores": [
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5"
+	]
+},
+{
+	"name": "Shine Toriama",
+	"photo": "https://i.ytimg.com/vi/jvImc0s1QVY/maxresdefault.jpg",
+	"scores": [
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5"
+	]
+},
+{
+	"name": "Shine Toriama",
+	"photo": "https://i.ytimg.com/vi/jvImc0s1QVY/maxresdefault.jpg",
+	"scores": [
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5"
+	]
+},
+{
+	"name": "Shay",
+	"photo": "https://www.pinterest.com/pin/169799848429179364/",
+	"scores": [
+	"2",
+	"4",
+	"1",
+	"3",
+	"2",
+	"2",
+	"1",
+	"1",
+	"1",
+	"2"
+	]
+},
+{
+	"name": "Collin",
+	"photo": "google.com",
+	"scores": [
+	"5",
+	"3",
+	"4",
+	"1",
+	"5",
+	"1",
+	"5",
+	"1",
+	"5",
+	"4"
+	]
+},
+{
+	"name": "Collin",
+	"photo": "google.com",
+	"scores": [
+	"5",
+	"3",
+	"4",
+	"1",
+	"5",
+	"1",
+	"5",
+	"1",
+	"5",
+	"4"
+	]
+},
+{
+	"name": "Collin",
+	"photo": "google.com",
+	"scores": [
+	"5",
+	"3",
+	"4",
+	"1",
+	"5",
+	"1",
+	"5",
+	"1",
+	"5",
+	"4"
+	]
+},
+{
+	"name": "Bob",
+	"photo": "https://static-cdn.jtvnw.net/jtv_user_pictures/bobross-profile_image-0b9dd167a9bb16b5-300x300.jpeg",
+	"scores": [
+	"5",
+	"4",
+	"2",
+	"1",
+	"4",
+	"3",
+	"4",
+	"2",
+	"3",
+	"2"
+	]
+},
+{
+	"name": "Bob",
+	"photo": "https://static-cdn.jtvnw.net/jtv_user_pictures/bobross-profile_image-0b9dd167a9bb16b5-300x300.jpeg",
+	"scores": [
+	"5",
+	"4",
+	"2",
+	"1",
+	"4",
+	"3",
+	"4",
+	"2",
+	"3",
+	"2"
+	]
+},
+{
+	"name": "Bob",
+	"photo": "https://static-cdn.jtvnw.net/jtv_user_pictures/bobross-profile_image-0b9dd167a9bb16b5-300x300.jpeg",
+	"scores": [
+	"5",
+	"4",
+	"2",
+	"1",
+	"4",
+	"3",
+	"4",
+	"2",
+	"3",
+	"2"
+	]
+},
+{
+	"name": "ygj",
+	"photo": "oighio",
+	"scores": [
+	"3",
+	"3",
+	"4",
+	"3",
+	"3",
+	"3",
+	"5",
+	"4",
+	"3",
+	"2"
+	]
+},
+{
+	"name": "bob",
+	"photo": "https://google.com",
+	"scores": [
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1"
+	]
+},
+{
+	"name": "John Snow",
+	"photo": "http://www.act.org/content/dam/act/unsecured/Images/the-act-test-prep-practice-book-250x250-Aug-2015.png",
+	"scores": [
+	"5",
+	"4",
+	"4",
+	"5",
+	"3",
+	"5",
+	"5",
+	"5",
+	"5",
+	"2"
+	]
+},
+{
+	"name": "Harrison Farrison",
+	"photo": "http://www.petguide.com/wp-content/uploads/2016/11/2FluffyDog.jpg",
+	"scores": [
+	"3",
+	"3",
+	"3",
+	"3",
+	"3",
+	"3",
+	"3",
+	"3",
+	"3",
+	"3"
+	]
+},
+{
+	"name": "test",
+	"photo": "test",
+	"scores": [
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1"
+	]
+},
+{
+	"name": "srehserhe",
+	"photo": "sfgsd",
+	"scores": [
+	"2",
+	"2",
+	"4",
+	"3",
+	"2",
+	"5",
+	"3",
+	"2",
+	"2",
+	"2"
+	]
+}];
+
+module.exports = friendMatches;
--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-// Add the application routes
+
 require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
 require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 
-// Start listening on PORT
+
 app.listen(PORT, function() {
   console.log(" App is listening on PORT: " + PORT);
 });
